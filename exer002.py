4df2466a78a15a7f0670d6c1f56f875cbc7a3e6a
Mauricio <mauricio_olicer75@live.com>
2023-05-29 18:39:31 -0300

    exercicio2

---
+++ exer002.py
@@ -0,0 +1,4 @@
+'2)Crie um programa, onde o usuário digite o seu nome e logo em seguida'
+'de uma mensagem de boas vindas'
+nome = input('Digite o seu nome:')
+print('Olá {}, Seja bem-vindo!'.format(nome))
