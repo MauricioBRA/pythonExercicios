cd8da7e2d3967d30213560a42afea5e3cb8532be
Mauricio <mauricio_olicer75@live.com>
2023-06-12 15:07:01 -0300

    Melhorias do exercicio 4

--- exer004.py
+++ exer004.py
@@ -1,8 +1,11 @@
 #4)Faça um programa que leia algo pelo teclado e mostre na tela o sseu tipo
 #primitivo e todas as informações possiveis sobre ela.
 v1 = input('digite algo: ')
-print('Tipo da classe', type(v1),
-      'é númerico? {}'.format(v1.isnumeric()),
-      'é alpha numérico? {}'.format(v1.isalnum()),
-      'é alfabético? {}'.format(v1.isalpha()),
-      'Todas as letras é maiúsculas? {}'.format(v1.isupper()))
+print('Tipo da classe', type(v1))
+print('É númerico? {}'.format(v1.isnumeric()))
+print('É alpha numérico? {}'.format(v1.isalnum()))
+print('É alfabético? {}'.format(v1.isalpha()))
+print('Todas as letras está em maiúsculas? {}'.format(v1.isupper()))
+print('Todas as letras está em minúsculas? {}'.format(v1.islower()))
+print('Possui espaço? {}'.format(v1.isspace()))
+print('Está capitalizada? {}'.format(v1.istitle()))
